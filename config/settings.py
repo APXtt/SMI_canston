be6383243386e26c7b5d0d59083a5648620d7d41
apxtt <dfwtt11@gmail.com>
2023-06-08 15:51:42 +0900

    d

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'staticroot/' # 배포할 때 staticroot로 바꾸셈
+STATIC_URL = '/staticroot/' # 배포할 때 staticroot로 바꾸셈
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
